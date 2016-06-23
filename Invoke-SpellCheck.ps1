ad49ed2c37c99b1e6467e14a9c52534387e9940d
Prateek Singh <Prateeksingh1590@gmail.com>
2016-06-23 18:08:10 +0530

    Update Invoke-SpellCheck.ps1

--- Invoke-SpellCheck.ps1
+++ Invoke-SpellCheck.ps1
@@ -1,4 +1,41 @@
-﻿Function Invoke-SpellCheck
+<#.Synopsis
+Identify and Rectify spelling mistakes in an input String
+.DESCRIPTION
+Identify spelling meistakes and repeated token in a string and suggests possible combination of correct spellings, able to identify Nouns in the string and converts first alphabet in Uppercase.
+Cmdlet is Using Microsoft cognitive service's "Spell Check" API as a service to get the information needed by making HTTP calls to the API
+NOTE : You need to subscribe the "Spell Check API" before using the powershell script from the following link and setup an environment variable like, $env:MS_SpellCheck_API_key = "YOUR API KEY"
+    
+API Subscription Page - https://www.microsoft.com/cognitive-services/en-US/subscriptions
+.EXAMPLE
+Hello world
+.EXAMPLE
+PS D:\> "owershell is is fun" | Invoke-SpellCheck -ShowErrors |ft -AutoSize
+
+ErrorToken Type          Suggestions
+---------- ----          -----------
+owershell  UnknownToken  powershell 
+is         RepeatedToken         
+
+DESCRIPTION
+-----------
+When Invoke-SpellCheck function is used with -ShowErrors switch, it identifies unknown tokens (mistakes), repeated token in the string and displays Suggestions.  
+.EXAMPLE
+PS D:\> "thes is the the graet wall of china" |Invoke-SpellCheck
+this is the great wall of China
+the is the great wall of China
+
+DESCRIPTION
+-----------
+Using the Suggestion function generates all possible combination of sentences.
+.EXAMPLE
+PS D:\> "b!ll g@tes" |Invoke-SpellCheck -RemoveSpecialChars
+Bill Gates
+
+DESCRIPTION
+-----------
+When Invoke-SpellCheck function is used with -RemoveSpecialChars switch, it removes all special character fomr the input string and rectifies the spelling mistakes.
+#>
+Function Invoke-SpellCheck
 {
 [CmdletBinding()]
 Param(
@@ -24,7 +61,7 @@ Process{
             Method = 'Post'
 			}
 
-            $Headers =  @{'Ocp-Apim-Subscription-Key' = "0a774da790a941b59c4104c0c2f61a44"}
+            $Headers =  @{'Ocp-Apim-Subscription-Key' = $env:MS_SpellCheck_API_key}
 			$body =     @{'text'= $s}
             Try{
                 $SpellingErrors = (Invoke-RestMethod @SplatInput -Headers $Headers -Body $body ).flaggedTokens
@@ -74,12 +111,3 @@ End
 {
 }
 }
-
-"effery snove" | Invoke-SpellCheck
-
-"Owershell", "detrimination" |Invoke-SpellCheck
-
-"Owershell", "detrimination" |Invoke-SpellCheck -ShowErrors
-
-"this is is great w@ll of china" |Invoke-SpellCheck -RemoveSpecialChars -ShowErrors
-
