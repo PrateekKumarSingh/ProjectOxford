c1efcbcb9b2b964ebedba09705a0d8730c77371a
Prateek Kumar Singh <prateekkumar.singh@markit.com>
2018-04-17 13:32:42 +0530

    added -FromAzureRM switch to set-localCongnitiveServiceConfiguration cmdlet

--- Functions/Set-LocalCognitiveServiceConfiguration.ps1
+++ Functions/Set-LocalCognitiveServiceConfiguration.ps1
@@ -2,23 +2,54 @@ Function Set-LocalCognitiveServiceConfiguration {
     [alias("config")]
     [cmdletbinding()]
     param(
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ParameterSetName='Manual')]
         [ValidateNotNullOrEmpty()]
         [String] $SubscriptionKey,
-        [Parameter(Mandatory)] [Location] $Location,
-        [Parameter(Mandatory)] [CognitiveService] $ServiceName
+        [Parameter(Mandatory, ParameterSetName='Manual')]
+        [Location] $Location,
+        [Parameter(Mandatory, ParameterSetName='Manual')]
+        [CognitiveService] $ServiceName,
+        [Parameter(ParameterSetName='FromAzure')] 
+        [switch] $FromAzureRM
     )
 
-    Write-Verbose "Setting Environment variables" 
-    Set-Item -Path "env:API_SubscriptionKey_$ServiceName" -Value $SubscriptionKey
-    Set-Item -Path "env:API_Location_$ServiceName" -Value $Location
-
-    [System.Object] [Ordered] @{
-        SubscriptionKey = $SubscriptionKey
-        Location        = $Location
-        Service         = $ServiceName
-        Endpoint        = "https://$Location.api.cognitive.microsoft.com/$ServiceName/v1.0/"
+    $VerboseEnvVariableSetup = [scriptblock]{
+        Write-Verbose "Setting Environment variable: `$env:API_SubscriptionKey_$ServiceName for Cognitive Service: $ServiceName" 
+        Set-Item -Path "env:API_SubscriptionKey_$ServiceName" -Value $SubscriptionKey
+        Write-Verbose "Setting Environment variable: `$env:API_Location_$ServiceName for Cognitive Service: $ServiceName" 
+        Set-Item -Path "env:API_Location_$ServiceName" -Value $Location
     }
+
+    if(Test-AzureRmLogin){
+        Get-AzureRmCognitiveServicesAccount | ForEach-Object {
+            $SubscriptionKey = ($_ | Get-AzureRmCognitiveServicesAccountKey).Key1
+            $Location        = $_.Location
+            $ServiceName     = ([uri]$_.Endpoint).segments[1] -replace "/",""
+            $EndPoint        = $_.Endpoint
+            
+            $data = [System.Object] [Ordered] @{
+                SubscriptionKey =   $SubscriptionKey 
+                Location        =   $Location       
+                ServiceName     =   $ServiceName    
+                EndPoint        =   $EndPoint       
+            }
+
+            if($data){
+                & $VerboseEnvVariableSetup
+            }
+        }
+    }
+    else {        
+            & $VerboseEnvVariableSetup
+            
+        [System.Object] [Ordered] @{
+            SubscriptionKey = $SubscriptionKey
+            Location        = $Location
+            Service         = $ServiceName
+            Endpoint        = "https://$Location.api.cognitive.microsoft.com/$ServiceName/v1.0/"
+        }
+    }
+
 }
 
-#Set-LocalCognitiveServiceConfiguration -SubscriptionKey 'bd59f063c3a5437c96377c7684a0aaad' -Location southeastasia -ServiceName vision -verbose
+Set-LocalCognitiveServiceConfiguration -FromAzureRM -verbose
