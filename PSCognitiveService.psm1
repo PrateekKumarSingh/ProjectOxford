1c586c4d0ad32f2ee85869459ad00ae3c0303e80
Prateek Singh <prateeksingh1590@gmail.com>
2018-04-19 03:49:25 +0530

    refactoring and code fix

--- PSCognitiveService.psm1
+++ PSCognitiveService.psm1
@@ -1,6 +1,8 @@
 using assembly System.Drawing
 [cmdletbinding()]
 param()
+
+# define class sequence
 $classList = @(
     'Enum',
     'ValidateFile',
@@ -8,21 +10,13 @@ $classList = @(
     'ComputerVision'
 )
 
+# importing classes sequentially
 foreach ($class in $classList) {
     Write-Verbose "Class '$class'" -Verbose
     . "$psscriptroot\classes\$class.ps1"
 }
 
-# Dot Sourcing files
+# dot dourcing files
 Get-ChildItem $PSScriptRoot\private\ -Recurse  | Where-Object {$_.Extension -eq '.ps1' -and $_.Directory -notlike '*x'} | ForEach-Object {. $_.FullName}
 Get-ChildItem $PSScriptRoot\public\ -Recurse  | Where-Object {$_.Extension -eq '.ps1' -and $_.Directory -notlike '*x'} | ForEach-Object {. $_.FullName }
 
-#Get-ImageAnalysis -path 'C:\tmp\Bill.jpg' -Verbose
-#analyze 'C:\tmp\Bill.jpg' -Verbose
-#Get-ImageAnalysis 'C:\tmp\Bill.jpg' -VisualFeatures Faces, Color -Details Celebrities -Verbose
-#Get-ImageAnalysis -URL https://pbs.twimg.com/profile_images/963507920016216064/Ug29J5-J_400x400.jpg -Verbose
-#man Get-ImageAnalysis -full
-
-
-
-###Invoke-Expression $([System.IO.File]::ReadAllText('.\Classes\ComputerVision.ps1'))
--- Public/New-LocalConfiguration.ps1
+++ Public/New-LocalConfiguration.ps1
@@ -1,4 +1,4 @@
-Function Set-LocalCognitiveServiceConfiguration {
+Function New-LocalConfiguration {
     [alias("config")]
     [cmdletbinding()]
     param(
@@ -10,7 +10,7 @@ Function Set-LocalCognitiveServiceConfiguration {
         [Parameter(Mandatory, ParameterSetName='Manual')]
         [CognitiveService] $ServiceName,
         [Parameter(ParameterSetName='FromAzure')] 
-        [switch] $FromAzureRM
+        [switch] $FromAzure
     )
 
     $VerboseEnvVariableSetup = [scriptblock]{
@@ -19,14 +19,33 @@ Function Set-LocalCognitiveServiceConfiguration {
         Write-Verbose "Setting Environment variable: `$env:API_Location_$ServiceName for Cognitive Service: $ServiceName" 
         Set-Item -Path "env:API_Location_$ServiceName" -Value $Location
     }
-
-    if(Test-AzureRmLogin -verbose){
-        Get-AzureRmCognitiveServicesAccount | ForEach-Object {
-            $SubscriptionKey = ($_ | Get-AzureRmCognitiveServicesAccountKey).Key1
-            $Location        = $_.Location
-            $ServiceName     = ([uri]$_.Endpoint).segments[1] -replace "/",""
-            $EndPoint        = $_.Endpoint
-            
+    
+    if($FromAzure){
+        if(Test-AzureRmLogin -verbose){
+            Write-Verbose "Fetching AzureRM Cognitive Service accounts" 
+            $Accounts = Get-AzureRmCognitiveServicesAccount 
+            Write-Verbose $("{0} Service found in AzureRM" -f $Accounts.Count)
+            $Accounts | ForEach-Object {
+                $SubscriptionKey = ($_ | Get-AzureRmCognitiveServicesAccountKey).Key1
+                $Location        = $_.Location
+                $ServiceName     = ([uri]$_.Endpoint).segments[1] -replace "/",""
+                $EndPoint        = $_.Endpoint
+                
+                $data = [System.Object] [Ordered] @{
+                    SubscriptionKey =   $SubscriptionKey 
+                    Location        =   $Location       
+                    ServiceName     =   $ServiceName    
+                    EndPoint        =   $EndPoint       
+                }
+    
+                if($data){
+                    & $VerboseEnvVariableSetup
+                    $data
+                }
+            }
+        }
+    }
+    else{
             $data = [System.Object] [Ordered] @{
                 SubscriptionKey =   $SubscriptionKey 
                 Location        =   $Location       
@@ -38,8 +57,7 @@ Function Set-LocalCognitiveServiceConfiguration {
                 & $VerboseEnvVariableSetup
                 $data
             }
-        }
     }
 }
 
-#Set-LocalCognitiveServiceConfiguration -FromAzureRM -verbose
+#New-LocalConfiguration -FromAzure -verbose
