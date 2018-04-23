e7c4730b4b7da37f41a146fb3eca5c2dfed3c95e
Prateek Singh <prateeksingh1590@gmail.com>
2018-04-24 01:18:51 +0530

    function to expose class instances and method overload definitions outside the nested PowerShell module

--- PSCognitiveService.psm1
+++ PSCognitiveService.psm1
@@ -30,3 +30,22 @@ $FolderNames = @(
 )
 
 Get-ChildItem $($FolderNames.ForEach({"$BasePath\$_\"})) -Recurse -Filter *.ps1 | ForEach-Object {. $_.FullName}
+
+# function to expose class instances and method overload definitions
+# outside the nested PowerShell module
+Function New-CognitiveServiceInstance{
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory)]
+        [ValidateSet('ComputerVision','Face','ContentModerator')] $Name
+    )
+
+    switch($Name){
+        
+        'ComputerVision' {[ComputerVision]::new($env:API_SubscriptionKey_ComputerVision, $env:API_Location_ComputerVision)}
+        'Face' {[Face]::new($env:API_SubscriptionKey_Face, $env:API_Location_Face)}
+        'ContentModerator' {[face]::new($env:API_SubscriptionKey_ContentModerator, $env:API_Location_ContentModerator)}
+    }
+
+}
+
