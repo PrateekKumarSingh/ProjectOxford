fae2d80586d3dba48c89233825a7ba63477edc1a
Prateek Kumar Singh <prateekkumar.singh@markit.com>
2018-04-23 16:28:28 +0530

    updated new-localconfig fucntion to add keys to profile

--- Public/New-LocalConfiguration.ps1
+++ Public/New-LocalConfiguration.ps1
@@ -10,14 +10,23 @@ Function New-LocalConfiguration {
         [Parameter(Mandatory, ParameterSetName='Manual')]
         [CognitiveService] $ServiceName,
         [Parameter(ParameterSetName='FromAzure')] 
-        [switch] $FromAzure
+        [switch] $FromAzure,
+        [Parameter(ParameterSetName='Manual')]
+        [Parameter(ParameterSetName='FromAzure')] 
+        [switch] $AddKeysToProfile
     )
 
-    $VerboseEnvVariableSetup = [scriptblock]{
-        Write-Verbose "Setting Environment variable: `$env:API_SubscriptionKey_$ServiceName for Cognitive Service: $ServiceName" 
+    $Configuration = [scriptblock]{
+        param([switch]$AddKeysToProfile)
+        Write-Verbose "Setting Environment variable: `$env:API_SubscriptionKey_$ServiceName for Cognitive Service: $($ServiceName.ToString().ToUpper())" 
         Set-Item -Path "env:API_SubscriptionKey_$ServiceName" -Value $SubscriptionKey
-        Write-Verbose "Setting Environment variable: `$env:API_Location_$ServiceName for Cognitive Service: $ServiceName" 
-        Set-Item -Path "env:API_Location_$ServiceName" -Value $Location
+        Write-Verbose "Setting Environment variable: `$env:API_Location_$ServiceName for Cognitive Service: $($ServiceName.ToString().ToUpper())" 
+        Set-Item -Path "env:API_Location_$ServiceName" -Value $Location -Verbose
+        if($AddKeysToProfile){
+            Write-Verbose "Adding Environment variable(s) to Profile: $Profile" -Verbose
+            Add-Content $profile -Value "`$env:API_SubscriptionKey_$ServiceName = '$SubscriptionKey'"
+            Add-Content $profile -Value "`$env:API_Location_$ServiceName = '$Location'"
+        }
     }
     
     if($FromAzure){
@@ -25,40 +34,40 @@ Function New-LocalConfiguration {
             Write-Verbose "Already Logged in." 
             Write-Verbose "Fetching AzureRM Cognitive Service accounts" 
             $Accounts = Get-AzureRmCognitiveServicesAccount 
-            Write-Verbose $("{0} Service found in AzureRM [] " -f $Accounts.Count)
-            $Accounts | ForEach-Object {
-                $SubscriptionKey = ($_ | Get-AzureRmCognitiveServicesAccountKey).Key1
-                $Location        = $_.Location
-                $ServiceName     = ([uri]$_.Endpoint).segments[1] -replace "/",""
-                $EndPoint        = $_.Endpoint
-                
-                $data = [System.Object] [Ordered] @{
-                    ServiceName     =   $ServiceName    
-                    Location        =   $Location       
-                    SubscriptionKey =   $SubscriptionKey 
-                    EndPoint        =   $EndPoint       
-                }
-    
-                if($data){
-                    & $VerboseEnvVariableSetup
+            Write-Verbose $("{0} Service found in AzureRM [{1}] " -f $Accounts.Count, $($Accounts.AccountType -join ', ') ) -Verbose
+            if($Accounts){
+                $Accounts | ForEach-Object {
+                    $SubscriptionKey = ($_ | Get-AzureRmCognitiveServicesAccountKey).Key1
+                    $Location        = $_.Location
+                    $ServiceName     = ([uri]$_.Endpoint).segments[1] -replace "/",""
+                    $EndPoint        = $_.Endpoint
+                    
+                    $data = [System.Object] [Ordered] @{
+                        ServiceName     =   $ServiceName    
+                        Location        =   $Location       
+                        SubscriptionKey =   $SubscriptionKey 
+                        EndPoint        =   $EndPoint       
+                    }
+
+                    if($AddKeysToProfile){ & $Configuration -addKeysToProfile }
+                    else{ & $Configuration }
+
                     $data
                 }
             }
         }
     }
     else{
-            $data = [System.Object] [Ordered] @{
-                ServiceName     =   $ServiceName    
-                Location        =   $Location       
-                SubscriptionKey =   $SubscriptionKey 
-                EndPoint        =   $EndPoint       
-            }
-
-            if($data){
-                & $VerboseEnvVariableSetup
-                $data
-            }
+        $data = [System.Object] [Ordered] @{
+            ServiceName     =   $ServiceName    
+            Location        =   $Location       
+            SubscriptionKey =   $SubscriptionKey 
+        }
+        
+        if($AddKeysToProfile){ & $Configuration -addKeysToProfile }
+        else{ & $Configuration }
+        $data 
     }
-}
 
+}
 #New-LocalConfiguration -FromAzure -verbose
