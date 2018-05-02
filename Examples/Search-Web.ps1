78c84b2f389ed14be022e780e04e65f62eea0402
Prateek Singh <prateeksingh1590@gmail.com>
2018-05-03 04:50:33 +0530

    !deploy

--- Examples/Search-Web.ps1
+++ Examples/Search-Web.ps1
@@ -1,11 +1,14 @@
+# simple bing search using PowerShell
 Search-Web powershell
 bing microsoft
-New-CognitiveServiceInstance -Name BingSearchV7 -Verbose
 
-bing powershell | % webpages | % value | select name, snippet, url 
+# instantiating Cognitive service instance for Bing Search
+$Bing = New-CognitiveServiceInstance -Name BingSearchV7
+$Bing.Search('powershell 6.2').webpages.value | select name, url 
 
+
+# more examples with parameters -SafSearch, -Count, -Offset
 $r = Search-Web -SafeSearch moderate -Text 'git' -Count 13 -Verbose -OffSet 2
 $r = Search-Web -Text 'adult' -Count 5 -Verbose -Market 'English-United States' -SafeSearch off
 $r = Search-Web -Text 'adult' -Count 5 -Verbose -Market 'English-United States' -SafeSearch strict
-$r.webPages.value | measure
 $r.webPages.value | select name, url
