275cd1b2e15fe7cb574efcc014e92036675bbc8d
Prateek Singh <prateeksingh1590@gmail.com>
2018-06-23 20:19:57 +0530

    !deploy

--- build.ps1
+++ build.ps1
@@ -4,10 +4,10 @@ param($Task = 'Default')
 # Grab nuget bits, install modules, set build variables, start build.
 "  Install Dependent Modules"
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-Install-Module Psake, PSDeploy, Pester, BuildHelpers -Force
+Install-Module Psake, PSDeploy, Pester, BuildHelpers, AzureRM.Profile, AzureRM.Profile.NetCore, AzureRM.CognitiveServices.NetCore, AzureRM.CognitiveServices -Force
 
 "  Import Dependent Modules"
-Import-Module Psake, BuildHelpers, Pester
+Import-Module Psake, BuildHelpers, Pester, AzureRM.Profile, AzureRM.Profile.NetCore, AzureRM.CognitiveServices.NetCore, AzureRM.CognitiveServices
 
 Set-BuildEnvironment -GitPath "C:\Program Files\Git\bin\git.exe"
 
