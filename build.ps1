98f66351600867c7fcd95a34cde781e2f146eeed
Prateek Singh <prateeksingh1590@gmail.com>
2018-06-23 20:56:50 +0530

    !deploy

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ param($Task = 'Default')
 # Grab nuget bits, install modules, set build variables, start build.
 "  Install Dependent Modules"
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-Install-Module Psake, PSDeploy, Pester, BuildHelpers
+Install-Module Psake, PSDeploy, Pester, BuildHelpers -Force
 Install-MOdule -Name 'AzureRM.Profile','AzureRM.CognitiveServices','AzureRM.Profile.NetCore','AzureRM.CognitiveServices.NetCore' -Force -AllowClobber
 
 "  Import Dependent Modules"
