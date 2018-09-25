ceba90b032c4be84630c8661b0e689840dc9cbde
Raphael <raphael.faeh@corporatesoftware.ch>
2018-09-25 03:52:59 +0200

    Add Test-FaceMatch

---
+++ PSCognitiveService/Public/Face/Test-FaceMatch.ps1
@@ -0,0 +1,24 @@
+Function Test-FaceMatch {
+    [alias("Face")]
+    [cmdletbinding()]
+    param(
+        [System.Guid]$referenceFaceId,
+        [System.Guid]$differenceFaceId
+    )
+
+    begin {
+    }
+    process {
+        $Object = @()
+        if (Test-LocalConfiguration -ServiceName 'Face') {            
+            $Object = [Face]::new($env:API_SubscriptionKey_Face, $env:API_Location_Face)
+            $Object.verify($referenceFaceId,$differenceFaceId)    
+        }
+        Remove-Variable -Name Object
+    }
+    end {
+    }
+
+}
+
+Export-ModuleMember -Function Get-Face -Alias tfm
