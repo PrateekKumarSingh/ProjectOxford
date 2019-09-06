35f8876e247a034d0f6326af91b34d8b5b079ca1
PrateekKumarSingh <PrateekSingh1590@gmail.com>
2019-09-07 04:16:09 +0530

    !deploy

--- build.ps1
+++ build.ps1
@@ -4,8 +4,8 @@ param($Task = 'Default')
 # Grab nuget bits, install modules, set build variables, start build.
 "  Install Dependent Modules"
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-Get-Module AzureRM* -ListAvailable | Uninstall-Module -Force -Verbose -Confirm:$false
-Get-Module az* -ListAvailable
+# Get-Module AzureRM* -ListAvailable | Uninstall-Module -Force -Verbose -Confirm:$false
+# Get-Module az* -ListAvailable
 Install-Module Psake, PSDeploy, Pester, BuildHelpers, az.Profile, az.CognitiveServices, az.Resources -Force -AllowClobber
 
 "  Import Dependent Modules"
