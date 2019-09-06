844b0e944882218f9a28f70377cc614df263bd6d
PrateekKumarSingh <PrateekSingh1590@gmail.com>
2019-09-07 03:59:13 +0530

    !deploy

--- build.ps1
+++ build.ps1
@@ -4,12 +4,12 @@ param($Task = 'Default')
 # Grab nuget bits, install modules, set build variables, start build.
 "  Install Dependent Modules"
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-Uninstall-AzureRm -Verbose
 Install-Module Psake, PSDeploy, Pester, BuildHelpers, az.Profile, az.CognitiveServices, az.Resources -Force -AllowClobber
 
 "  Import Dependent Modules"
 Import-Module Psake, PSDeploy, Pester, BuildHelpers, az.Profile, az.CognitiveServices, az.Resources
 
+Uninstall-AzureRm -Verbose
 Set-BuildEnvironment -GitPath "C:\Program Files\Git\bin\git.exe"
 
 "  InvokeBuild"
